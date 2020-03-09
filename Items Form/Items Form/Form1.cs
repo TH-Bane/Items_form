328d74690069d8a362fb20899193643df4626c26
Tyler Hern <thern92@gmail.com>
2020-03-08 21:41:25 -0500

    Add Menu and Exit

--- Items Form/Items Form/Form1.cs
+++ Items Form/Items Form/Form1.cs
@@ -16,5 +16,10 @@ namespace Items_Form
         {
             InitializeComponent();
         }
+
+        private void exitToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
     }
 }
